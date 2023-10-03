06a6d729e85a4696ae735b7528b951d4d5e62a3a
isaservienski <145451992+isaservienski@users.noreply.github.com>
2023-10-03 08:38:44 -0300

    adicionando arquivos

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);  
+    let palavra= "Isabelle"
+    textAlign(CENTER,CENTER);
+    text(palavra,200,200)
+   
+     let maxímo=width
+    let minimo=0
+    //mouseX,0,width
+    
+    
+  }
